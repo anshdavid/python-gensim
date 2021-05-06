ff45a2496bf7718bd605759426da597cf697a7cc
Ansh David <ansh1990@gmail.com>
2021-05-06 13:55:45 -0400

    initial commit

---
+++ src/utils.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm, gridspec
+from sklearn.manifold import TSNE
+
+
+def plot_with_labels(lowDWeights, labels):
+    plt.cla()
+    X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+    for x, y, s in zip(X, Y, labels):
+        cmap = cm.get_cmap("rainbow")
+        c = cmap(int(255 * s / 9))
+        plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+    plt.xlim(X.min(), X.max())
+    plt.ylim(Y.min(), Y.max())
+    plt.show()
+    plt.pause(1)
+
+
+def data_viz(layer, label):
+    # https://github.com/MorvanZhou/PyTorch-Tutorial/blob/master/tutorial-contents-notebooks/401_CNN.ipynb
+    tsne = TSNE(perplexity=30, n_components=2, init="pca", n_iter=5000)
+    plot_only = 500
+    low_dim_embs = tsne.fit_transform(layer.data.numpy()[:plot_only, :])
+    labels = label.numpy()[:plot_only]
+    plot_with_labels(low_dim_embs, labels)
+
+
+def plot_test_image_result(img, ps, le, cam=None):
+
+    _ = plt.figure(figsize=(8, 6))
+    gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1])
+    ax1, ax2 = plt.subplot(gs[0]), plt.subplot(gs[1])
+
+    if cam is not None:
+        ax1.imshow(cam, alpha=0.6)
+        ax1.imshow(img, alpha=0.4)
+    else:
+        ax1.imshow(img)
+
+    ax2.barh(np.arange(len(ps)), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(len(ps)))
+
+    for i, v in enumerate(ps):
+        ax2.text(0.01, i - 0.1, f"{v:.3f}", color="blue", fontweight="bold")
+
+    if le is None:
+        ax2.set_yticklabels(np.arange(len(ps)))
+    else:
+        ax2.set_yticklabels(le.inverse_transform(np.arange(len(ps))))
+
+    ax2.set_title("Class Probability")
+    ax2.set_xlim(0, 1)
+
+    plt.tight_layout()
+    plt.show()
